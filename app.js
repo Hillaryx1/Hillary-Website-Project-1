ea2694cc3db9642ce15ff1725f1fdd3583e2d5b2
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-03-11 22:43:05 -0400

    Second Commit

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".navLinks");
 const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close-btn");
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
     overlay.classList.toggle("open"); 
 });
+
+closeBtn.addEventListener("click", () => { // Add an event listener to the close button
+    navLinks.classList.remove("open");
+    overlay.classList.remove("open");
+});
+
 
 
